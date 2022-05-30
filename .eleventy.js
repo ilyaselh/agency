98d1b64d70df45b65d83b16edf048236e1c24abd
Ilyas <ilyaselh@gmail.com>
2022-05-30 12:43:00 +0100

    icon

--- .eleventy.js
+++ .eleventy.js
@@ -4,6 +4,7 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("./src/img");
   eleventyConfig.addPassthroughCopy("./src/js");
   eleventyConfig.addPassthroughCopy("./src/admin");
+  eleventyConfig.addPassthroughCopy("./src/Favicon");
     // Return your Object options:
     return {
       dir: {
