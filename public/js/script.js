f3e94d995f2fa18791bf092c369d1985f7be51e9
ilyas <ilyaselh@gmail.com>
2024-10-10 10:57:04 +0100

    ga4

--- public/js/script.js
+++ public/js/script.js
@@ -1,2 +1,9 @@
 
-console.log("working from script")
+function toggleForm() {
+    var form = document.getElementById("quoteForm");
+    if (form.style.display === "none" || form.style.display === "") {
+      form.style.display = "block";
+    } else {
+      form.style.display = "none";
+    }
+  }
